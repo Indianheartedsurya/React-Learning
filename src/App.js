f2d05cfdaec62f8d4dff4e6f1d7d20ecefff7d2c
Suraj Maurya <mnaveen1888@gmail.com>
2023-08-20 16:27:33 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello Dunia
-     
+    <>
+    <div className="mainContainer">
+      <h2 className ="header">Resume</h2>
     </div>
+   <div className="container">
+    
+   </div>
+
+
+    </>
   );
 }
-
 export default App;
